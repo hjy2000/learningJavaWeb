0d292c4a13dfab71c756a45f08abe5a236927444
hjy2000 <1034104198@qq.com>
2022-08-09 18:09:54 +0800

    20220809-01-work

--- mavenTest1/src/main/java/com/itheima/mapper/BrandMapper.java
+++ mavenTest1/src/main/java/com/itheima/mapper/BrandMapper.java
@@ -1,12 +1,30 @@
 package com.itheima.mapper;
 
 import com.itheima.pojo.Brand;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
+import java.util.Map;
 
 public interface BrandMapper {
-    /*
-    * 查询所有
-    * */
     public List<Brand> selectAll();
+    Brand selectById(int id);
+
+    /**
+     * 条件查询 三种参数接收方式
+     * 1.散装参数：方法中若有多个参数 需要使用@Param("SQL参数占位符名称")要严格对应
+     * 2.对象参数： 对象的属性名要和参数占位符严格对应
+     * 3.map集合参数：
+     * @param status
+     * @param companyname
+     * @param brandname
+     * @return
+     */
+    List<Brand> selectByCondition(@Param("status")int status,@Param("companyName") String companyname,@Param("brandName") String brandname);
+    List<Brand> selectByCondition1(Brand brand);
+    List<Brand> selectByCondition2(Map map);
+
+    List<Brand> selectByCondition3(Map map);
+    List<Brand> selectByConditionSingle(Brand brand);//单条件动态查询
+    void add(Brand brand);
 }
--- mavenTest1/src/test/java/MybatisTest1.java
+++ mavenTest1/src/test/java/MybatisTest1.java
@@ -8,10 +8,11 @@ import org.junit.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MybatisTest1 {
-
     @Test
     public void testsekectAll() throws IOException {
         //1. 加载mybatis的核心配置文件，获取 SqlSessionFactory
@@ -28,5 +29,93 @@ public class MybatisTest1 {
         //4. 释放资源
         sqlSession.close();
     }
+    @Test
+    public void testSelectById() throws IOException {
+        int id=1;
+        //1. 加载mybatis的核心配置文件，获取 SqlSessionFactory
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
 
+        //2. 获取SqlSession对象，用它来执行sql
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        //3
+        BrandMapper brandmapper = sqlSession.getMapper(BrandMapper.class);//获取代理对象
+        Brand brand= brandmapper.selectById(id);//通过接口执行xml对应的sql
+        System.out.println(brand);
+        //4. 释放资源
+        sqlSession.close();
+    }
+    @Test
+    public void testSelectByCondition() throws IOException {
+        int status=1;
+        String companyName="华为";
+        String brandName="华为";
+        //处理参数 在程序完成
+        companyName = "%" + companyName + "%";
+        brandName = "%" + brandName + "%";
+
+        //封装brand对象给参数方法二使用
+        Brand brand=new Brand();
+        brand.setStatus(status);
+//        brand.setCompanyName(companyName);
+//        brand.setBrandName(brandName);
+
+        //封装map集合给参数方法三使用
+        Map map=new HashMap();
+//        map.put("status",status);
+        map.put("companyName",companyName);
+//        map.put("brandName",brandName);
+
+        //1. 加载mybatis的核心配置文件，获取 SqlSessionFactory
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+
+        //2. 获取SqlSession对象，用它来执行sql
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        //3
+        BrandMapper brandmapper = sqlSession.getMapper(BrandMapper.class);//获取代理对象
+//        List<Brand> brands= brandmapper.selectByCondition(status,companyName,brandName);//通过接口执行xml对应的sql
+//        List<Brand> brands1= brandmapper.selectByCondition1(brand);//通过接口执行xml对应的sql
+//        List<Brand> brands2= brandmapper.selectByCondition2(map);//通过接口执行xml对应的sql
+//        List<Brand> brands3= brandmapper.selectByCondition3(map);
+        List<Brand> brands4= brandmapper.selectByConditionSingle(brand);
+        System.out.println(brands4);
+        //4. 释放资源
+        sqlSession.close();
+    }
+    @Test
+    public void testAdd() throws IOException {
+        int status = 1;
+        String companyName="波导手机";
+        String brandName="波导";
+        String description="手机中的战斗机";
+        int ordered = 100;
+
+        //封装brand对象
+        Brand brand=new Brand();
+        brand.setStatus(status);
+        brand.setCompanyName(companyName);
+        brand.setBrandName(brandName);
+        brand.setDescription(description);
+        brand.setOrdered(ordered);
+
+        //1. 加载mybatis的核心配置文件，获取 SqlSessionFactory
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+
+        //2. 获取SqlSession对象，用它来执行sql
+        SqlSession sqlSession = sqlSessionFactory.openSession(true);//可以设置传参自动提交事务（true）
+        //3
+        BrandMapper brandmapper = sqlSession.getMapper(BrandMapper.class);//获取代理对象
+        brandmapper.add(brand);
+
+        //提交事务
+//        sqlSession.commit();
+
+        //4. 释放资源
+        sqlSession.close();
+    }
 }
